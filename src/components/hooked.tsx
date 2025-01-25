1b5af1920415114c80b8cb4a42e654f448cbe75f
prabhat <prabhatshukla0849@gmail.com>
2025-01-25 13:42:26 +0530

    hooked post

--- src/components/hooked.tsx
+++ src/components/hooked.tsx
@@ -29,3 +29,18 @@ const RandomColorBackground = () => {
 };
 
 export default RandomColorBackground;
+
+
+
+
+
+export default function Post(){
+  return <>
+    <div style= {{display:"flex" , backgroundColor:"#0D1117", color:"white", height:"100vh" , width:"100vw"}}>
+      <div style= {{fontSize:'100px' , marginLeft:'20px'}}> hello</div>
+      <div style= {{fontSize:'100px' , marginLeft:'20px'}}> hello</div>
+
+    </div>
+      <h1 style={{fontFamily:'cursive', color:'#333'}}>Prabhat shukla</h1>
+  </>
+}
