53db49327bc0b26028532b88427d16cde3f4443e
prabhat <prabhatshukla0849@gmail.com>
2025-02-01 15:19:42 +0530

    abortrequest methodology

--- src/components/fetch.tsx
+++ src/components/fetch.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 
 const BASE = "https://jsonplaceholder.typicode.com";
@@ -12,22 +12,64 @@ const Make = () =>{
 
   const [posts , setPosts] = useState<POST[]>()
 
+  const [error , setError] = useState()
+
+  const [loading , setIsloading] = useState(false)
+
+  const [page , setPage] = useState(0);
+
+  const abortControllerRef = useRef<AbortController|null>(null)
+
   useEffect(()=>{
 
     const  fetchPosts = async() =>{
-      const response = await fetch(`${BASE}/posts`)
-      const data = await response.json()
-      setPosts(data)
+     
+      abortControllerRef.current?.abort();
+      abortControllerRef.current = new AbortController
+
+      setIsloading(true)
+
+      try{
+        const response = await fetch(`${BASE}/posts?page=${page}`,{
+          signal: abortControllerRef.current?.signal,
+        })
+        const data = (await response.json()) as POST[];
+        setPosts(data)
+      }catch(e:any){
+        if(e.name === 'AbortError'){
+          console.log("Aborted")
+          return;
+        }
+
+        setError(e)
+      }finally{
+        setIsloading(false)
+      }
+     
+      
     }
 
     fetchPosts()
-  },[])
+  },[page])
 
-  return <div>
-    <div></div>
-    {posts?.map((post)=>{
+ 
+  if(error){
+    return <div> SomeThing went wrong</div>
+  }
+  return <div className="min-h-[200px]">
+    <h1>Posts</h1>
+    <button onClick={()=>{
+      setPage(page+1)
+    }}>INCREASE:{page}</button>
+    
+    {loading&&<div>LOADING...</div>}
+
+     {!loading&&
+     <ul>
+      {posts?.map((post)=>{
       return <li key={post.id}>{post.title}</li>
     })}
+     </ul>}
 
   </div>
   
